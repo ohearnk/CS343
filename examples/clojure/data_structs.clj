cfbc602d0fbbf9f268d70c5aff36bef67cb2c426
ohearnk <kohearn1989@yahoo.com>
2013-04-10 13:51:21 -0400

    Added more data structure stuff for Clojure.

--- examples/clojure/data_structs.clj
+++ examples/clojure/data_structs.clj
@@ -1,3 +1,4 @@
+; some basic functions and type checking
 (= 1 2)
 (class true)
 (class (= 2 2))
@@ -12,7 +13,7 @@
 (if nil (println "nil is boolean true") (println "nil is boolean false"))
 (println "")
 
-; lists and common functions associated with lists
+; lists (ordered collections of homogeneous data types) and common functions associated with lists
 (println (list 1 2 3))
 (println (first '(1 2 3)))
 (println (rest '(1 2 3)))
@@ -21,7 +22,7 @@
 (println (cons 4 '(1 2 3)))
 (println "")
 
-; vectors: data structures optimized for random access
+; vectors: ordered collections of homogeneous data types optimized for random access
 ;   (by using an offset as opposed to traversing the data
 ;   structure done in lists)
 (println (nth [1 2 3] 1))
@@ -34,3 +35,27 @@
 (println board)
 (println (nth board 1))
 (println (nth (nth board 1) 1))
+(println "")
+
+; sets: unordered collections of homogeneous data types
+(println '#{"peach" "pear" "apple"})
+(def fruit #{"peach" "pear" "apple"})
+(println (sort fruit))
+(println (sorted-set 3 5 7 2 3 1))
+;(println (clojure.set/union #{1 4 3} #{1 2 5}))
+;(println (clojure.set/difference #{1 4 3} #{1 2 5}))
+; set membership test
+(println (#{"peach" "pear" "apple"} "pear"))
+(println (#{"peach" "pear" "apple"} "lemon"))
+(println "")
+
+; maps
+; note: in Clojure, strings prepended with a colon are
+; called keywords (essentially, creates a common reference for
+; all uses of the keyword)
+(println {:soft "peach" :mushy "pear" :hard "apple"})
+(def fruits {:soft "peach" :mushy "pear" :hard "apple"})
+(println (fruits :hard))
+; also valid, strangely
+(println (:hard fruit))
+(println (merge {:bob "616-555-1212", :chuck "515-555-1212"} {:alice "331-555-1212"}))
