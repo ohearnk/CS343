9e241679683c06903c21e5a829f07528fca30b7a
Nick Olesak <olesakn@mail.gvsu.edu>
2013-04-15 20:02:14 -0400

    "Removed old tic_tac_Toe project.

--- projects/project4_tic_tac_toe/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to project4_tic_tac_toe
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- projects/project4_tic_tac_toe/test/project4_tic_tac_toe/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns project4-tic-tac-toe.core-test
-  (:use clojure.test
-        project4-tic-tac-toe.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
